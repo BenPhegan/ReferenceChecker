b7575d62573784204a4e8d549265e0bd83c931d4
Ben Phegan <benphegan@gmail.com>
2012-10-24 23:02:22 +1100

    Moving stuff around.

---
+++ ReferenceChecker.Tests/AssemblyReferenceGrapherTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Mono.Cecil;
+using NSubstitute;
+using NUnit.Framework;
+using ReferenceChecker.Gac;
+
+namespace ReferenceChecker.Tests
+{
+    [TestFixture]
+    public class AssemblyReferenceGrapherTests
+    {
+        [Test]
+        public void FirstTest()
+        {
+            var assembly = AssemblyDefinition.CreateAssembly(new AssemblyNameDefinition("Test", new Version("1.0")), "Test", ModuleKind.Dll);
+            assembly.MainModule.AssemblyReferences.Add(new AssemblyNameReference("Blah", new Version("1.0")));
+            var stream = new MemoryStream();
+            assembly.Write(stream);
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+                {
+                    {@"c:\test.dll", new MockFileData(stream.ToArray())}
+                });
+
+            var gacResolver = Substitute.For<IGacResolver>();
+            string output;
+            gacResolver.AssemblyExists(Arg.Any<String>(),out output).Returns(true);
+            var grapher = new AssemblyReferenceGrapher(fileSystem, gacResolver);
+            var graph = grapher.GenerateAssemblyReferenceGraph(new List<Regex>(), new ConcurrentBag<string> {@"c:\test.dll"}, false);
+            Assert.AreEqual(2,graph.Vertices.Count());
+        }
+    }
+}
---
+++ ReferenceChecker.Tests/GacResolverTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using ReferenceChecker.Gac;
+
+namespace ReferenceChecker.Tests
+{
+    [TestFixture]
+    public class GacResolverTests
+    {
+        [TestCase("System", Result = true, Description = "Can resolve System")]
+        [TestCase("Giberishshsidfasdfasdfasdf.asdfasdfas.dasdfasdf", Result = false, Description = "Cant resolve gibberish")]
+        [TestCase("System.Xml, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089, ProcessorArchitecture=MSIL", Result = true, Description = "Using full name")]
+        [TestCase("System.Xml, Version=4.0.0.0, Culture=neutral, PublicKeyToken=null, ProcessorArchitecture=MSIL", Result = true, Description = "With a null PublicKeyToken, we return false rather than throw exception")]
+        public bool CanResolveSystem(string assemblyName)
+        {
+            string test;
+            var resolver = new GacResolver();
+            return resolver.AssemblyExists(assemblyName, out test);
+        }
+    }
+}
